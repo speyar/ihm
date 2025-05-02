3aa0910b495508b100be3255c7d5308ff427d58e
Karim <127413175+pywolf1503@users.noreply.github.com>
2025-05-02 18:48:56 +0100

    initial repo

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import {HeaderSection} from "@/sections/header-section";
+
+export default function Home() {
+  return (
+    <main className="bg-black">
+      <HeaderSection></HeaderSection>
+      <section className="h-screen"></section>
+      <section className="h-screen"></section>
+      <section className="h-screen"></section>
+    </main>
+  );
+}
---
+++ src/sections/header-section.tsx
@@ -0,0 +1,11 @@
+export function HeaderSection() {
+    return (
+        <section>
+            <div className="p-7">
+                <img src="/img_pack/1.jpg" alt="IMAGE 1" className="w-full h-screen object-cover" />
+                <img src="/img_pack/2.jpg" alt="IMAGE 2" className="w-full h-screen object-cover" />
+                <img src="/img_pack/3.jpg" alt="IMAGE 3" className="w-full h-screen object-cover" />
+            </div>
+        </section>
+    );
+}
